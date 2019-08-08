e29297d5265fdad50dfc2bb8da7e998cd7b4468c
Kevin <juns139@uw.edu>
2019-08-08 10:26:20 -0700

    first commit

---
+++ app/src/main/java/android/bignerdranch/com/MainActivity.java
@@ -0,0 +1,24 @@
+package android.bignerdranch.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragments();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+    }
+}
