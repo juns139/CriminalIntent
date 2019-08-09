1e133e067870305568759b260632c8710bad208e
Kevin <juns139@uw.edu>
2019-08-08 22:57:20 -0700

    start of chapter

---
+++ app/src/main/java/android/bignerdranch/com/CrimeLab.java
@@ -0,0 +1,18 @@
+package android.bignerdranch.com;
+
+import android.content.Context;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+
+    }
+}
